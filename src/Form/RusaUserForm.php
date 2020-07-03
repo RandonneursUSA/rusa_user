8d7745cfd16dde4dce951ea6c1b9f4c20073b45e
Paul Lieberman <lieb77@gmail.com>
2020-07-02 20:56:08 -0700

    adds a confirmation screen

--- src/Form/RusaUserForm.php
+++ src/Form/RusaUserForm.php
@@ -8,19 +8,19 @@
 
 namespace Drupal\rusa_user\Form;
 
-use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\ConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger;
+use Drupal\Core\Url;
 use Drupal\user\Entity\User;
 use Drupal\user\Entity\Role;
 use Drupal\rusa_api\RusaMembers;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class RusaUserForm extends FormBase {
+class RusaUserForm extends ConfirmFormBase {
 
-    protected $memobj; // API Member object
+    protected $member;   // API Member object
     protected $settings; // From our config form
-
+    protected $step = 1; // Keep track of multistep form
     /**
      * Required
      *
@@ -40,13 +40,41 @@ class RusaUserForm extends FormBase {
         
     }   
 
+   /**
+    * {@inheritdoc}
+    */
+    public function getCancelUrl() {
+        return new Url('user.register');
+    }
 
+    /**
+    * {@inheritdoc}
+    */
+    public function getQuestion() {
+        $name = $this->member->fname . ' ' . $this->member->sname;
+        
+        return $this->t("Are you %name?", ['%name' => $name]);        
+    }
+    
+    /**
+    * {@inheritdoc}
+    */
+    public function getDescription() {
+        return $this->t("Please confirm your identity.");
+    }
+    
+    
     /**
      * {@inheritdoc}
      *
      */
     public function buildForm(array $form, FormStateInterface $form_state) {       
- 
+    
+        // If we're at the confirmation step just pass it on
+        if ($this->step === 2) {
+            return parent::buildForm($form, $form_state);
+        }
+        
         // Build the form
         $form['info'] = [
             '#type'    => 'item',
@@ -54,18 +82,14 @@ class RusaUserForm extends FormBase {
         ];
 
         $form['mid'] = [
-            '#type'   => 'textfield',
-            '#title'  => $this->t('RUSA #'),
-            '#size'   => '20',
+            '#type'     => 'textfield',
+            '#title'    => $this->t('RUSA #'),
+            '#size'     => '20',
+            '#required' => TRUE,
         ];
 
          $form['actions'] = [
-            '#type'   => 'actions',
-                'cancel'  => [
-                    '#type'  => 'submit',
-                    '#value' => 'Cancel',
-                    '#attributes' => ['onclick' => 'if(!confirm("Do you really want to cancel?")){return false;}'],
-                ],
+            '#type'   => 'actions',                
                 'submit' => [
                     '#type'  => 'submit',
                     '#value' => 'Submit',
@@ -74,70 +98,78 @@ class RusaUserForm extends FormBase {
     
         $form['#attached']['library'][] = 'rusa_api/rusa_style';
     
-    
-    
         return $form;
     }
 
     public function validateForm(array &$form, FormStateInterface $form_state) {
 
+        // The confirmation step needs no additional validation.
+        if ($this->step === 2) {
+            return;
+        }
+
+
         // See if we have a value
         $mid = $form_state->getValue('mid');
         if (!empty($mid)) {
+        
+            // Get member and titles from the RUSA database
+            $memobj  = new RusaMembers(['key' => 'mid', 'val' => $mid]); 
+            $memobj->addTitles();
+        
             // Check to see if user with this ID already exists
             if ($this->checkExisting($mid)) {
                 $form_state->setErrorByName('mid', "A user with RUSA # " . $mid . " already exists.");
             }
 
-            // Get volunteer titles from the RUSA database
-            $this->memobj  = new RusaMembers(['key' => 'mid', 'val' => $mid]); 
-            $this->memobj->addTitles();
-
             // Check for valid ID
-            if (! $this->memobj->isValid($mid)) {
+            if (! $memobj->isValid($mid)) {
                 $form_state->setErrorByName('mid', $mid . " does not appear to be a valid RUSA #.");
             }
             // Check for expired user
-            if ($this->memobj->isExpired($mid)) {
+            if ($memobj->isExpired($mid)) {
                 $form_state->setErrorByName('mid', "Membership for RUSA # " . $mid . " is expired.");
             }
+            
+            // Member data is good so save it
+            $this->member = $memobj->getMember($mid);
         }
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
-        $action = $form_state->getTriggeringElement();
-        if ($action['#value'] == "Cancel") {
-            $form_state->setRedirect('rusa.home');
+   
+        // Don't submit the form until after confirmation
+        if ($this->step === 1) {
+            $form_state->setRebuild();
+            $this->step = 2;
+            return;
         }
-        else {
-            // Add the user 
-            $mid    = $form_state->getValue('mid');
-            $member = $this->memobj->getMember($mid);
-            $email  = $member->email;
-            $url    = $this->addUser($member);
-            
+              
+        // Add the user 
+        $email = $this->member->email;
+        $url   = $this->addUser();
+        
 
-            $to = 'rusa'; // $email Will be member's email
-            
-            $params = [ 
-                'from'    => $this->settings['acctmgr'],  
-                'message' => $this->settings['notify'],
-                'url'     => $url,
-                'subj'    => 'Login link for rusa.org',
-            ];
+        $to = 'rusa'; // $email Will be member's email
+        
+        $params = [ 
+            'from'    => $this->settings['acctmgr'],  
+            'message' => $this->settings['notify'],
+            'url'     => $url,
+            'subj'    => 'Login link for rusa.org',
+        ];
 
-            $mail = \Drupal::service('plugin.manager.mail');
-            $result = $mail->mail('rusa_user', 'notify', $to, 'en', $params, $reply = NULL, $send = TRUE);
+        $mail = \Drupal::service('plugin.manager.mail');
+        $result = $mail->mail('rusa_user', 'notify', $to, 'en', $params, $reply = NULL, $send = TRUE);
 
-            // Status message
-            $message = $this->settings['message'];
-            
-            // Eventually use the token service with a custom token handler but for now
-            $message = str_replace('[rusa:mid]', $mid, $message);
-            
-            $this->messenger()->addStatus($message);            
-            $form_state->setRedirect('rusa.home');
-        }
+        // Status message
+        $message = $this->settings['message'];
+        
+        // Eventually use the token service with a custom token handler but for now
+        $message = str_replace('[rusa:mid]', $this->member->mid, $message);
+        
+        $this->messenger()->addStatus($message);            
+        $form_state->setRedirect('rusa.home');
     }
 
     private function checkExisting($mid) {
@@ -149,7 +181,10 @@ class RusaUserForm extends FormBase {
         return $uids;
     }
 
-    private function addUser($udata) {
+    private function addUser() {
+    
+        $udata = $this->member;
+        
         // Create the user
         $user = User::create();
 
